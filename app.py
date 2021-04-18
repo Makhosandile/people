7c9625cc2483ef3de18fa9a1410d32c4269ce683
Makhosandile <mpnyamela@gmail.com>
2021-04-18 09:00:55 +0200

    Added the people

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-import connection
-
+from flask import render_template
+import connexion
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./')
@@ -14,11 +13,10 @@ def home():
     """
     This function just responds to the browser ULR
     localhost:5000/
-
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
